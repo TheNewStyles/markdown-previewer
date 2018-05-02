91a07a56c1b1613d19ea3845d6d11a3be4e6321b
TheNewStyles <tyler.peterson@utah.edu>
2018-05-01 19:05:00 -0500

    Add callback method to send state to parent

---
+++ src/components/MarkdownOutput.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export class MarkdownOutput extends Component {
+    // Get Markdown from app
+    // Convert Raw Markdown
+    // Display
+    
+    render() {
+      return (
+        <div className='item'>
+  
+        </div>
+      );
+    }
+  }
+  
