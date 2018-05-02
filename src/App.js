14a3d1090502d8d2c31522b2b1d90a5340842a31
TheNewStyles <tyler.peterson@utah.edu>
2018-05-01 19:03:28 -0500

    Add function to pass state from child to parent and vice versa

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { MarkdownInput } from './components/MarkdownInput';
+import { MarkdownOutput } from './components/MarkdownOutput';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: 'Heading\n=======\n'
+    };
+  }
 
-  
+  onMarkdownEntered(newMarkdown) {
+    this.setState({
+      markdown: newMarkdown
+    });
+  }
+
   render() {
     return (
       <div className='container'>
-        <MarkdownInput newText={this.updateText}/>
+        <MarkdownInput
+          initialMarkdown={this.state.markdown}
+          updateMarkdown={this.onMarkdownEntered.bind(this)}
+          markdown={this.state.markdown} />
+        {/* TODO send markdown to output */}
         <MarkdownOutput />
       </div>
     )
-  }
-}
-
-class MarkdownInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      value: 'SHIT'
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});       
-  }
-
-  render() {
-    return (
-      <div className='item'>
-        <textarea id="input"
-          value={this.state.value} 
-          onChange={this.handleChange}/>
-      </div>
-    );
-  }
-}
-
-class MarkdownOutput extends Component {
-  render() {
-    return (
-      <div className='item'>
-
-      </div>
-    );
   }
 }
 
